014f46587ac9ab98e7f33b03b70e9c0293cd756d
Louis <colletlouis@gmail.com>
2021-08-26 14:16:29 +0200

    First page done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,89 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="home">
+    <div class="head-info">
+      <p class="title">Welcome!</p>
+      <p class="sm-text">
+        You currently have
+        <strong>{{ personalDetails.length }}</strong> Friend(s).
+      </p>
+    </div>
+    <div class="friends-area">
+      <p class="sm-title">Friend(s)</p>
+      <div class="friends">
+        <div
+          class="friend"
+          v-for="personalDetail in personalDetails"
+          :key="personalDetail.id"
+        >
+          <img
+            v-if="personalDetail.image"
+            :src="imageUrlFor(personalDetail.image)"
+          />
+          <p>{{ personalDetail.firstname }} {{ personalDetail.lastname }}</p>
+          <router-link :to="`/personalDetails/${personalDetail.slug.current}`">
+            <button>View</button>
+          </router-link>
+        </div>
+      </div>
+    </div>
   </div>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import sanity from "@/client";
+import imageUrlBuilder from "@sanity/image-url";
 
-#nav {
-  padding: 30px;
+const imageBuilder = imageUrlBuilder(sanity);
+const query = `*[_type == "personalDetails"]{
+  _id,
+  firstname,
+  lastname,
+  slug,
+  "image": profileImage{
+  asset->{
+  _id,
+  url
 }
+}
+}[0...50]`;
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: "Home",
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data() {
+    return {
+      loading: true,
+
+      personalDetails: [],
+    };
+  },
+
+  created() {
+    this.fetchData();
+  },
+
+  methods: {
+    imageUrlFor(source) {
+      return imageBuilder.image(source);
+    },
+
+    fetchData() {
+      this.error = this.personalDetail = null;
+
+      this.loading = true;
+
+      sanity.fetch(query).then(
+        (personalDetails) => {
+          this.loading = false;
+
+          this.personalDetails = personalDetails;
+        },
+
+        (error) => {
+          this.error = error;
+        }
+      );
+    },
+  },
+};
+</script>
---
+++ src/client.js
@@ -0,0 +1,8 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+  projectId: "bxvlir7r",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "v2021-08-26",
+});
