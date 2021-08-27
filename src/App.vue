d246b593221ea1ca9b0e0df2547859ea7b8b7e7f
Louis <colletlouis@gmail.com>
2021-08-27 12:51:08 +0200

    Details page

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,54 @@
 <template>
-  <div class="home">
-    <div class="head-info">
-      <p class="title">Welcome!</p>
-      <p class="sm-text">
-        You currently have
-        <strong>{{ personalDetails.length }}</strong> Friend(s).
-      </p>
-    </div>
-    <div class="friends-area">
-      <p class="sm-title">Friend(s)</p>
-      <div class="friends">
-        <div
-          class="friend"
-          v-for="personalDetail in personalDetails"
-          :key="personalDetail.id"
-        >
-          <img
-            v-if="personalDetail.image"
-            :src="imageUrlFor(personalDetail.image)"
-          />
-          <p>{{ personalDetail.firstname }} {{ personalDetail.lastname }}</p>
-          <router-link :to="`/personalDetails/${personalDetail.slug.current}`">
-            <button>View</button>
-          </router-link>
-        </div>
-      </div>
-    </div>
-  </div>
+  <router-view />
 </template>
 
-<script>
-import sanity from "@/client";
-import imageUrlBuilder from "@sanity/image-url";
-
-const imageBuilder = imageUrlBuilder(sanity);
-const query = `*[_type == "personalDetails"]{
-  _id,
-  firstname,
-  lastname,
-  slug,
-  "image": profileImage{
-  asset->{
-  _id,
-  url
+<style>
+#app {
+  font-family: "Poppins", sans-serif;
+  background-color: #e5e5e5;
+  min-height: 100vh;
+  height: 100%;
+  max-width: 100%;
 }
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
-}[0...50]`;
-
-export default {
-  name: "Home",
-
-  data() {
-    return {
-      loading: true,
-
-      personalDetails: [],
-    };
-  },
-
-  created() {
-    this.fetchData();
-  },
-
-  methods: {
-    imageUrlFor(source) {
-      return imageBuilder.image(source);
-    },
-
-    fetchData() {
-      this.error = this.personalDetail = null;
-
-      this.loading = true;
-
-      sanity.fetch(query).then(
-        (personalDetails) => {
-          this.loading = false;
-
-          this.personalDetails = personalDetails;
-        },
-
-        (error) => {
-          this.error = error;
-        }
-      );
-    },
-  },
-};
-</script>
+.header {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: center;
+  background-color: #5c3fd7;
+  color: #fff;
+  padding: 10px;
+}
+.header p {
+  padding: 10px;
+}
+img {
+  max-width: 100%;
+}
+.icon {
+  transition: 0.5s;
+}
+.router-link-exact-active .icon {
+  transform: scale(1.2) translateY(-7px);
+}
+.bottom-area {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 76px;
+  box-shadow: 0px 0 3px rgba(143, 143, 143, 0.8);
+  z-index: 2;
+}
+</style>
